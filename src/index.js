6e6abd153a49f4d023b56cf26ac49682ff8352d6
Srihari <Srhrsrihari@gmail.com>
2019-09-23 17:54:20 +0000

    UI changes

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import calorieData from "./data/calorie-data";
 import "./styles.css";
 
 function TotalCalory({ totalCalorie }) {
-  return <h2> Your total calorie for today: {totalCalorie}</h2>;
+  return (
+    <h2 className="card-title">
+      {" "}
+      Your total calorie for today: {totalCalorie}
+    </h2>
+  );
 }
 function Header({ heading }) {
-  return <h1>{heading} </h1>;
+  return <h1 className="card-title">{heading} </h1>;
 }
 
 function FoodTable({ calorieData, foodPillClickHandler, itemcount }) {
@@ -28,7 +33,7 @@ function FoodTable({ calorieData, foodPillClickHandler, itemcount }) {
 class App extends React.Component {
   state = {
     totalCalorie: 0,
-    itemcount: 5
+    itemcount: 4
   };
   // Put the click handler here.
   foodPillClickHandler = totalCalorie => {
@@ -41,41 +46,42 @@ class App extends React.Component {
     this.setState({ totalCalorie: 0 });
   };
   btn3ClickHandler = itemcount => {
-    if (this.state.itemcount > 5) {
+    if (this.state.itemcount > 4) {
       this.setState({ itemcount: this.state.itemcount - 3 });
     }
   };
 
   render() {
     return (
-      <div className="card">
-        <div className="card-body">
-          <Header
-            className="card-title"
-            heading={"Let's see how many calories"}
-          />
-          <FoodTable
-            className="list-group"
-            calorieData={calorieData}
-            foodPillClickHandler={this.foodPillClickHandler}
-            itemcount={this.state.itemcount}
-          />
-          <br />
-          <TotalCalory
-            className="card-title"
-            totalCalorie={this.state.totalCalorie}
-          />
-          <button className="btn btn-primary" onClick={this.btn2ClickHandler}>
-            Load More Items
-          </button>
-          <span> </span>
-          <button className="btn btn-primary" onClick={this.btn3ClickHandler}>
-            Reduce Items
-          </button>
-          <span> </span>
-          <button className="btn btn-primary" onClick={this.btn1ClickHandler}>
-            Clear Count
-          </button>
+      <div>
+        <br />
+        <h1 className="text-primary">Cal-Cal</h1>
+        <div className="card">
+          <div className="card-body">
+            <Header heading={"Let's see how many calories"} />
+            <FoodTable
+              className="list-group"
+              calorieData={calorieData}
+              foodPillClickHandler={this.foodPillClickHandler}
+              itemcount={this.state.itemcount}
+            />
+            <br />
+            <TotalCalory
+              className="card-title"
+              totalCalorie={this.state.totalCalorie}
+            />
+            <button className="btn btn-primary" onClick={this.btn2ClickHandler}>
+              Load More Items
+            </button>
+            <span> </span>
+            <button className="btn btn-primary" onClick={this.btn3ClickHandler}>
+              Reduce Items
+            </button>
+            <span> </span>
+            <button className="btn btn-primary" onClick={this.btn1ClickHandler}>
+              Clear Count
+            </button>
+          </div>
         </div>
       </div>
     );
