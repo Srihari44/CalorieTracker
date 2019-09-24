040edb9b35d7ca304e0be004923377539553c3ee
Srihari <Srhrsrihari@gmail.com>
2019-09-24 18:51:46 +0000

    list-scroll

--- src/food-pill.js
+++ src/food-pill.js
@@ -2,10 +2,15 @@ import React from "react";
 
 function FoodPill({ name, measure, calories, onFoodPillClick }) {
   return (
-    <div className="list-group-item" onClick={() => onFoodPillClick(calories)}>
-      <span> {name} </span>
-      <span> {measure} </span>
-      <span> {calories} </span>
+    <div className="list-group">
+      <div
+        className="list-group-item"
+        onClick={() => onFoodPillClick(calories)}
+      >
+        <span> {name} </span>
+        <span> {measure} </span>
+        <span> {calories} </span>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import "./styles.css";
 
 function TotalCalory({ totalCalorie }) {
   return (
-    <h2 className="card-title">
-      {" "}
-      Your total calorie for today: {totalCalorie}
-    </h2>
+    <h2 className="card-title">Your total calorie for today: {totalCalorie}</h2>
   );
 }
 function Header({ heading }) {
@@ -18,37 +15,25 @@ function Header({ heading }) {
 }
 
 function FoodTable({ calorieData, foodPillClickHandler, itemcount }) {
-  return calorieData
-    .slice(0, itemcount)
-    .map(({ name, measure, calories }) => (
-      <FoodPill
-        key={name}
-        name={name}
-        measure={measure}
-        calories={calories}
-        onFoodPillClick={foodPillClickHandler}
-      />
-    ));
+  return calorieData.map(({ name, measure, calories }) => (
+    <FoodPill
+      key={name}
+      name={name}
+      measure={measure}
+      calories={calories}
+      onFoodPillClick={foodPillClickHandler}
+    />
+  ));
 }
 class App extends React.Component {
   state = {
-    totalCalorie: 0,
-    itemcount: 4
+    totalCalorie: 0
   };
-  // Put the click handler here.
   foodPillClickHandler = totalCalorie => {
     this.setState({ totalCalorie: this.state.totalCalorie + totalCalorie });
   };
-  btn2ClickHandler = itemcount => {
-    this.setState({ itemcount: this.state.itemcount + 3 });
-  };
   btn1ClickHandler = totalCalorie => {
     this.setState({ totalCalorie: 0 });
-  };
-  btn3ClickHandler = itemcount => {
-    if (this.state.itemcount > 4) {
-      this.setState({ itemcount: this.state.itemcount - 3 });
-    }
   };
 
   render() {
@@ -59,25 +44,18 @@ class App extends React.Component {
         <div className="card">
           <div className="card-body">
             <Header heading={"Let's see how many calories"} />
-            <FoodTable
-              className="list-group"
-              calorieData={calorieData}
-              foodPillClickHandler={this.foodPillClickHandler}
-              itemcount={this.state.itemcount}
-            />
+            <div className="group">
+              <FoodTable
+                calorieData={calorieData}
+                foodPillClickHandler={this.foodPillClickHandler}
+                itemcount={this.state.itemcount}
+              />
+            </div>
             <br />
             <TotalCalory
               className="card-title"
               totalCalorie={this.state.totalCalorie}
             />
-            <button className="btn btn-primary" onClick={this.btn2ClickHandler}>
-              Load More Items
-            </button>
-            <span> </span>
-            <button className="btn btn-primary" onClick={this.btn3ClickHandler}>
-              Reduce Items
-            </button>
-            <span> </span>
             <button className="btn btn-primary" onClick={this.btn1ClickHandler}>
               Clear Count
             </button>
